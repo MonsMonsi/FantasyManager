af432775ed9005883febef89c606251a4f70f681
LAPTOP-S0B8GD3M\tessn <tessnersimon@gmail.com>
2022-05-30 16:46:02 +0200

    .

--- FantasyManager.WPF/obj/Debug/net6.0-windows/Controls/NameSelectionView.g.i.cs
+++ FantasyManager.WPF/obj/Debug/net6.0-windows/Controls/NameSelectionView.g.i.cs
@@ -62,7 +62,7 @@ namespace FantasyManager.WPF.Controls {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/FantasyManager.WPF;component/controls/nameselectionview.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/FantasyManager.WPF;V1.0.0.0;component/controls/nameselectionview.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Controls\NameSelectionView.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
--- FantasyManager.WPF/obj/Debug/net6.0-windows/Controls/TutorialView.g.i.cs
+++ FantasyManager.WPF/obj/Debug/net6.0-windows/Controls/TutorialView.g.i.cs
@@ -62,7 +62,7 @@ namespace FantasyManager.WPF.Controls {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/FantasyManager.WPF;component/controls/tutorialview.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/FantasyManager.WPF;V1.0.0.0;component/controls/tutorialview.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Controls\TutorialView.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
